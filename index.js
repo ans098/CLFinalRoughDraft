4ab40ecd63b928e203e3f287cab794d25c6cfeb6
Alexis <as10281@nyu.edu>
2021-12-06 03:50:08 -0500

    updated draft

--- index.js
+++ index.js
@@ -6,22 +6,30 @@ let http = require('http');
 let server = http.createServer(app);
 let port = process.env.PORT || 3000;
 
-server.listen(port, ()=> {
+server.listen(port, () => {
     console.log('server is listening at ' + port);
 });
 
 let io = require('socket.io');
 io = new io.Server(server);
 
-io.sockets.on('connection', (socket)=> {
+io.sockets.on('connection', (socket) => {
     console.log('we have a new client: ' + socket.id);
 
-    socket.on('data', (data)=> {
+    socket.on('data', (data) => {
         // console.log(data);
-        io.sockets.emit('newBall', data);
+        // io.sockets.emit('newBall', data);
     });
 
-    socket.on('disconnect', ()=> {
+    socket.on('msg', (data) => {
+        console.log(data.name);
+        io.sockets.emit('msg', data);
+
+        //broadcast that there is a new user in the space
+        // socket.broadcast.emit('msg', data);
+    });
+
+    socket.on('disconnect', () => {
         console.log('client has disconnected: ' + socket.id);
     });
 });
--- public/sketch.js
+++ public/sketch.js
@@ -1,41 +1,62 @@
 let socket = io();
-let r,g,b;
+let r, g, b;
 let paddleL, paddleR, ball, topWall, botWall;
-let mouse;
 let speed = 10;
-let gameCounter;
-let scoreL = 0;
-let scoreR = 0;
+let score = 0;
+let chatBox = document.querySelector("#chatBoxMessages");
+let userName = document.querySelector("#chatUserName");
+let msg = document.querySelector("#chatInputMsg");
+let submitBtn = document.querySelector("#submitBtn");
+let newCanvas = document.querySelector("#canvas");
 
-socket.on('connect', ()=> {
+//------------------------------------------------------
+// code for sockets
+socket.on('connect', () => {
     console.log('connected');
 });
 
-// socket.on('newBall', (data)=> {
-//     // console.log(data);
-// });
+socket.on('newBall', (data) => {
+    console.log(data);
+    game(data);
+});
 
+//socket for chat function
+socket.on('msg', (data) => {
+    let newMessage = data.name + ":" + data.message;
+    let msgElement = document.createElement('p');
+    msgElement.innerHTML = newMessage;
+
+    chatBox.appendChild(msgElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+});
+
+//------------------------------------------------------
+//code for the game
 function setup() {
+    let name = prompt("Please enter your user name", "Name");
+    alert("Hello " + name + ". Welcome to Pong! \n\nThe rules of the game are simple. Use the paddle to stop the ball from hitting the wall on the right and left. Use your mouse to control both paddles. The ball will start moving automatically after you click ok, so make sure that you are ready to play once you click 'ok'. \n\nFeel free to pause the game at anytime to chat with others who are also playing the game or you can join a room with another player to play privately.\n\n Enjoy!");
+    
     createCanvas(700, 500);
+
     background('black');
 
     r = random(255);
     g = random(255);
     b = random(255);
 
-    paddleL = createSprite(50, height/2, 10, 100);
+    paddleL = createSprite(50, height / 2, 10, 100);
     paddleL.immovable = true;
 
-    paddleR = createSprite(width-50, height/2, 10, 100);
+    paddleR = createSprite(width - 50, height / 2, 10, 100);
     paddleR.immovable = true;
 
-    topWall = createSprite(width/2, -50/2, width, 50);
+    topWall = createSprite(width / 2, -50 / 2, width, 50);
     topWall.immovable = true;
 
-    botWall = createSprite(width/2, height+50/2, width, 50);
+    botWall = createSprite(width / 2, height + 50 / 2, width, 50);
     botWall.immovable = true;
 
-    ball = createSprite(width/2, height/2, 20, 20);
+    ball = createSprite(width / 2, height / 2, 20, 20);
     ball.maxSpeed = speed;
     ball.setSpeed(speed, -180);
 }
@@ -51,15 +72,10 @@ function draw() {
     // } else if (gameCounter == 1) {
     //     game();
     // }
+
+
     game();
 
-    fill('white');
-    textSize(30);
-    text("Score: " + scoreL, 20, 35);
-    text("Score: " + scoreR, width-150, 35);
-    strokeWeight(10);
-    stroke('white');
-    line(width/2,0, width/2, height);
 }
 
 // function gameIntro() {
@@ -83,9 +99,26 @@ function draw() {
 
 function game() {
     background('black');
+
+    fill('white');
+    textSize(30);
+    text("Score: " + score, 20, 35);
+    strokeWeight(10);
+    stroke('white');
+    line(width / 2, 0, width / 2, height);
+
+    let pause = createButton("Pause");
+    pause.position(120, 135);
+    pause.addEventListener('click', ()=> {
+        console.log('pause is clicked');
+    });
+
+    let leftPaddle = mouseY;
+    let rightPaddle = mouseY;
+
     //sets the paddle original positions and determine that they are followed by the user's mouse
-    paddleL.position.y = constrain(mouseY, paddleL.height/2, height-paddleL.height/2);
-    paddleR.position.y = constrain(mouseY, paddleR.height/2, height-paddleR.height/2);
+    paddleL.position.y = constrain(leftPaddle, paddleL.height / 2, height - paddleL.height / 2);
+    paddleR.position.y = constrain(rightPaddle, paddleR.height / 2, height - paddleR.height / 2);
 
     //has the ball bounce off the top and bottom of the canvas
     ball.bounce(topWall);
@@ -93,72 +126,54 @@ function game() {
 
     let changePos;
 
-
     //sets boundaries if ball collides with the paddles then it boucnes and changes direction
     if (ball.bounce(paddleL)) {
-        changePos = (ball.position.y - paddleL.position.y)/3;
-        ball.setSpeed(speed, ball.getDirection()+changePos);
-        scoreL +=1;
-        console.log(scoreL, scoreR);
+        changePos = (ball.position.y - paddleL.position.y) / 3;
+        ball.setSpeed(speed, ball.getDirection() + changePos);
+        score += 1;
+        // console.log(scoreL, scoreR);
     }
 
     if (ball.bounce(paddleR)) {
-        changePos = (ball.position.y - paddleR.position.y)/3;
-        ball.setSpeed(speed, ball.getDirection()-changePos);
-        scoreR +=1;
-        console.log(scoreL, scoreR);
+        changePos = (ball.position.y - paddleR.position.y) / 3;
+        ball.setSpeed(speed, ball.getDirection() - changePos);
+        score += 1;
+        // console.log(scoreL, scoreR);
     }
 
     //if ball hits the wall behind the paddles, the ball resets and the game starts over
-    if (ball.position.x<0) {
-        ball.position.x = width/2;
-        ball.position.y = height/2;
-        ball.setSpeed(speed, 0);
-        scoreL = 0;
+    if (ball.position.x < 0) {
+        ball.position.x = width / 2;
+        ball.position.y = height / 2;
+        ball.setSpeed(speed, 0)
+        score = 0;
     }
 
-    if (ball.position.x>width) {
-        ball.position.x = width/2;
-        ball.position.y = height/2;
+    if (ball.position.x > width) {
+        ball.position.x = width / 2;
+        ball.position.y = height / 2;
         ball.setSpeed(speed, 180);
-        scoreR = 0;
+        score = 0;
     }
 
     drawSprites();
-
-    // let newMousePos = {
-    //     y: mouseY
-    // }
-
-    // socket.emit('data', newMousePos);
 }
 
-// class Ball {
-//     constructor(x,y,col) {
-//         this.x = x;
-//         this.y = y;
-//         this.width = 40;
-//         this.height = 40;
-//         this.col = col;
-//     }
+function pause() {
+    console.log('pause clicked');
+    // ball.visible = false;
+}
 
-//     display() {
-//         fill(this.col);
-//         ellipse(this.x, this.y, this.width, this.height);
-//     }
+//-----------------------------------------------------
+//code for the chat box function
+submitBtn.addEventListener('click', ()=> {
+    let inputName = userName.value;
+    let inputMsg = msg.value;
+    let messageObject = {
+        "name": inputName,
+        "message": inputMsg
+    };
 
-//     newBall() {
-//         let newBallInfo = {
-//             x: random(50,windowWidth),
-//             y: random(50, windowHeight),
-//             width: this.width,
-//             height: this.height,
-//             red: r,
-//             green: g,
-//             blue: b, 
-//             mousePos: mouse
-//         };
+    socket.emit('msg', messageObject);
+});
 
-//         socket.emit('data', newBallInfo);
-//     }
-// }
